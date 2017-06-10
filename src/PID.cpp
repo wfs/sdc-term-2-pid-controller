02f3efec01c67c7158499720ec8472295ebb4657
andrew <hullpod.central@gmail.com>
2017-06-10 17:48:18 +1000

    pre-twiddle implementation

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,13 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    Kp = Kp;
+    Ki = Ki;
+    Kd = Kd;
+
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
